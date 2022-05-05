89e4c396c7ef784b2f9eb48df5f2ce6b7367f3a1
Akashdeep <rkrajput988@gmail.com>
2022-05-05 14:35:31 +0530

    adding rules for creating javascript variables

---
+++ js/index3.js
@@ -0,0 +1,31 @@
+console.log('index3');
+// variables in js
+// var, let, const
+var name1 = "Akash";
+var channel;
+var marks = 34;
+// channel = 'Rk Rajput'
+console.log(name1, channel, marks)
+
+// Rules for creating javascript variables
+/*
+1. can not start with numbers
+can start with letter, numbers, _ or $
+Are case sensitive
+*/
+
+var city = 'Delh'
+console.log(city)
+
+const OwnersName = 'Hari Ram' 
+console.log(OwnersName)
+const fruit = 'Apple'
+
+{
+    let city = 'Ram Pur'
+    console.log(city)
+}
+
+const arr1 = [12,23,12,53,3]
+// arr1.push(45)
+console.log(arr1)

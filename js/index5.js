85123cd929db8a9c1c15a1ccfaeb24eefd5be372
Akashdeep <rkrajput988@gmail.com>
2022-05-06 17:25:16 +0530

    adding type conversion and type coercion

--- js/index5.js
+++ js/index5.js
@@ -2,3 +2,17 @@
 
 console.log('welcome to index5')
 
+let myVar;
+myVar = String(34)
+// console.log(myVar, (typeof myVar))
+let booleanVar = String(true)
+console.log(booleanVar, typeof booleanVar)
+
+let date = String(new Date())
+console.log(date, (typeof date))
+
+let arr = String([1,2,3,4,5,6,7,8,9])
+console.log(arr.length, (typeof arr))
+
+let i = 75
+console.log(i.toString())
